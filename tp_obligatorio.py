e5e77886a3a270077ce4199538a187b4bf6f9122
JBE10 <jespino@uade.edu.ar>
2023-06-08 14:07:43 -0300

    Final

--- tp_obligatorio.py
+++ tp_obligatorio.py
@@ -1,29 +1,30 @@
-#def que sirve para promediar la cantidad de  tiempo cada camion, t_t=tiempo total c_v=cantidad de viajes hechos por ese numero de camion
-#lo primero que se hace es sacar el promedio en horas es decir que los datos recibidos dividido a la cantiad de viajes
-#luego esas horas se dividen por 24 ya que 24 horas=un dia y luego se utiliza la funcion resto(%) para sacar las horas que quedan
-#luego se devuelve la lista dias y horas ya promediada
+# def que sirve para promediar la cantidad de  tiempo cada camion, t_t=tiempo total c_v=cantidad de viajes hechos por ese numero de camion
+# lo primero que se hace es sacar el promedio en horas es decir que los datos recibidos dividido a la cantiad de viajes
+# luego esas horas se dividen por 24 ya que 24 horas=un dia y luego se utiliza la funcion resto(%) para sacar las horas que quedan
+# luego se devuelve la lista dias y horas ya promediada
 def cal_tiempo(t_t, c_v):
     hora_prom = []
     dias_horas_prom = []
     axuliar = 0
     ronda = 0
 
-    for i in t_t:
-        axuliar = i // c_v[ronda]
+    for i in range(len(t_t)):
+        axuliar = t_t[i] // c_v[ronda]
         hora_prom.append(axuliar)
         ronda += 1
 
-    for i in hora_prom:
-        dias = i // 24
+    for i in range(len(hora_prom)):
+        dias = hora_prom[i] // 24
 
-        horas = i % 24
+        horas = hora_prom[i] % 24
 
         dias_horas_prom.append([dias, "d", horas, "h"])
 
     return dias_horas_prom
 
-#esta funcion sirve para ordenar la lista de menor a mayor lo primero que se hace asignar un rango con len ya que el rango sera la cantidad de camiones
-#j es +1 de i ya que ira buscando una osicion mas adelante, el auxiliar ayudara a guaradar el item de la lista que esta siendo cambiado
+
+# esta funcion sirve para ordenar la lista de menor a mayor lo primero que se hace asignar un rango con len ya que el rango sera la cantidad de camiones
+# j es +1 de i ya que ira buscando una osicion mas adelante, el auxiliar ayudara a guaradar el item de la lista que esta siendo cambiado
 def orden_lista(a, b, c, d):
     for i in range(len(a) - 1):
         for j in range(i + 1, len(a)):
@@ -43,7 +44,9 @@ def orden_lista(a, b, c, d):
 
     lista = [a, b, c, d]
     return lista
-#programa principal en dodne se ingresar los datos y se almacenan
+
+
+# programa principal en dodne se ingresar los datos y se almacenan
 
 camiones = []
 distancia = []
@@ -60,7 +63,8 @@ while n_camion != -1:
 
     if n_camion < 0 or cant_horas < 0 or cant_km < 0 or cant_tn < 0:
         print("Datos Invalidos, introduccir datos positivos")
-#aca se busca si el dato del camion no esta repetido con un bucle while 
+    # aca se busca si el dato del camion no esta repetido con un bucle while , si lo esta se le suman los valores de km horas y toneladas, si el n camion
+    # no ecxistew en la lista se agrega
     else:
         encontrar = False
         i = 0
@@ -88,26 +92,28 @@ while n_camion != -1:
             cantidad.append(1)
 
     n_camion = int(input("Ingrese numero del camion: "))
-
+# se llama a la funcion para sacar el promedio de horas.
 hora_prom = cal_tiempo(horas, cantidad)
 
-
+# este if se utiliza para ver cuantos eleementos tiene la lsita, ya que si tiene solo un elemento no es necesario ordenarla
 if len(camiones) > 1:
     lista = orden_lista(camiones, distancia, toneladas, hora_prom)
-
+    # desarmamos la lista entragada por la funcion y creamos nuevas listas con el vactor de cada valor por ejemplo lista[0] se refiere al primer lugar
+    # de lista que corresponde a el numero de camiones
     lista_camiones = lista[0]
     lista_distancias = lista[1]
     lista_toneladas = lista[2]
     lista_horas = lista[3]
-    
+
     print("------------------------------------------------------------------------------------------------------------------------------------------------")
     print("============================================================================================")
     print("------N-Camiones------------Tiempo Promedio--------------Distancia Total-----------Carge Total")
     print("============================================================================================")
     print("------------------------------------------------------------------------------------------------------------------------------------------------")
     for i in range(len(lista_camiones)):
+        # Este if es para diferenciar los camiones con mas de 20000 km y agregarles un cartel de revision mecanica
         if lista_distancias[i] > 20000:
-            print("|        ",lista_camiones[i],"              ",lista_horas[i],"              ",lista_distancias[i],"Km","                 ",lista_toneladas[i],"Tn","  |Revisión mecánica",)
+            print("|        ",lista_camiones[i], "              ",lista_horas[i],"              ",lista_distancias[i],"Km","                 ",lista_toneladas[i],"Tn","  |Revisión mecánica",)
             print("------------------------------------------------------------------------------------------------------------------------------------------------")
         else:
             print("|        ",lista_camiones[i],"              ",lista_horas[i],"              ",lista_distancias[i],"Km","                 ",lista_toneladas[i],"Tn","  |",)
@@ -119,11 +125,12 @@ else:
     print("==================================================================================")
     print("------N-Camiones------------Tiempo Promedio--------------Distancia Total-----------Carge Total")
     print("==================================================================================")
-    print("------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("------------------------------------------------------------------------------------------------------------------------------------------------" )
+    # atravez de este for in se imprimiran todos los elementos de la lista con sus valores correspondentes
     for i in range(len(camiones)):
         if distancia[i] > 20000:
-            print("|        ",camiones[i],"              ",hora_prom[i],"           ",distancia[i],"Km","                  ",toneladas[i], "Tn","  |Revisión mecánica",)
+            print("|        ",camiones[i],"              ",hora_prom[i],"           ",distancia[i],"Km","                  ",toneladas[i],"Tn","  |Revisión mecánica",)
             print("------------------------------------------------------------------------------------------------------------------------------------------------")
         else:
-            print("|        ",camiones[i],"              ",hora_prom[i],"           ",distancia[i], "Km","                 ",toneladas[i],"Tn","  |",)
+            print("|        ",camiones[i],"              ",hora_prom[i],"           ",distancia[i],"Km","                 ",toneladas[i],"Tn","  |",)
             print("------------------------------------------------------------------------------------------------------------------------------------------------")
